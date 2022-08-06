7dcff7aa327d776043c6d57841bdc22e98165045
trickyrat <wangjian0513@outlook.com>
2022-08-07 01:34:33 +0800

    update: replace Chinese function name with English name

---
+++ src/util.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"strconv"
+	"strings"
+)
+
+const mask1, mask2 = 1 << 7, 1<<7 | 1<<6
+
+func max(a, b int) int {
+	if b > a {
+		return b
+	}
+	return a
+}
+
+func isSelfDividing(num int) bool {
+	for x := num; x > 0; x /= 10 {
+		if d := x % 10; d == 0 || num%d != 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func parseComplexNumber(num string) (real, imag int) {
+	i := strings.IndexByte(num, '+')
+	real, _ = strconv.Atoi(num[:i])
+	imag, _ = strconv.Atoi(num[i+1 : len(num)-1])
+	return
+}
+
+func getBytes(num int) int {
+	if num&mask1 == 0 {
+		return 1
+	}
+	n := 0
+	for mask := mask1; num&mask != 0; mask >>= 1 {
+		n++
+		if n > 4 {
+			return -1
+		}
+	}
+	if n >= 2 {
+		return n
+	}
+	return -1
+}
