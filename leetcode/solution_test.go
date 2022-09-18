cd93fc71107898602afe8756c9fee68cfb6e4401
trickyrat <wangjian0513@outlook.com>
2022-09-19 01:50:22 +0800

    add 1636. Sort Array by Increasing Frequency

--- leetcode/solution_test.go
+++ leetcode/solution_test.go
@@ -426,6 +426,12 @@ func TestMaxLengthBetweenEqualCharacters(t *testing.T) {
 	assert.Equal(t, -1, maxLengthBetweenEqualCharacters("bczyx"))
 }
 
+func TestFrequencySort(t *testing.T) {
+	assert.Equal(t, []int{3, 1, 1, 2, 2, 2}, frequencySort([]int{1, 1, 2, 2, 2, 3}))
+	assert.Equal(t, []int{1, 3, 3, 2, 2}, frequencySort([]int{2, 3, 1, 3, 2}))
+	assert.Equal(t, []int{5, -1, 4, 4, -6, -6, 1, 1, 1}, frequencySort([]int{-1, 1, -6, 4, 5, -6, 1, 4, 1}))
+}
+
 func TestFindTheWinner(t *testing.T) {
 	assert.Equal(t, 3, findTheWinner(5, 2))
 	assert.Equal(t, 1, findTheWinner(6, 5))
