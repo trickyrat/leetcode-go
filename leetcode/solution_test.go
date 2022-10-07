15759cae07a82c0f2eff6278eabebe02fc699093
trickyrat <wangjian0513@outlook.com>
2022-10-08 03:57:26 +0800

    test: add minAddToMakeValid test cases

--- leetcode/solution_test.go
+++ leetcode/solution_test.go
@@ -121,6 +121,11 @@ func TestReverseOnlyLetters(t *testing.T) {
 	assert.Equal(t, "Qedo1ct-eeLg=ntse-T!", reverseOnlyLetters("Test1ng-Leet=code-Q!"))
 }
 
+func TestMinAddToMakeValid(t *testing.T) {
+	assert.Equal(t, 1, minAddToMakeValid("())"))
+	assert.Equal(t, 3, minAddToMakeValid("((("))
+}
+
 func TestValidateStackSequences(t *testing.T) {
 	assert.Equal(t, true, validateStackSequences([]int{1, 2, 3, 4, 5}, []int{4, 5, 3, 2, 1}))
 	assert.Equal(t, false, validateStackSequences([]int{1, 2, 3, 4, 5}, []int{4, 3, 5, 1, 2}))
