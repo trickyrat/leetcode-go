92f21e64d4f73c6601e8e0b6933369585c7b8d41
trickyrat <wangjian0513@outlook.com>
2022-10-14 02:42:37 +0800

    feature: add 801. Minimum Swaps To Make Sequences Increasing

--- leetcode/solution.go
+++ leetcode/solution.go
@@ -727,6 +727,25 @@ func preimageSizeFZF(k int) int {
 	return nx(k+1) - nx(k)
 }
 
+// 801. Minimum Swaps To Make Sequences Increasing
+func minSwap(nums1, nums2 []int) int {
+	n := len(nums1)
+	a, b := 0, 1
+	for i := 1; i < n; i++ {
+		at, bt := a, b
+		a, b = n, n
+		if nums1[i] > nums1[i-1] && nums2[i] > nums2[i-1] {
+			a = min(a, at)
+			b = min(b, bt+1)
+		}
+		if nums1[i] > nums2[i-1] && nums2[i] > nums1[i-1] {
+			a = min(a, bt)
+			b = min(b, at+1)
+		}
+	}
+	return min(a, b)
+}
+
 // 804.Unique Morse Code Words
 func uniqueMorseRepresentations(words []string) int {
 	var morse = []string{
