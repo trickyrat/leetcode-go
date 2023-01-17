6cdd2f054e9533609ea9fb200c7c1fb4770be360
trickyrat <wangjian0513@outlook.com>
2023-01-17 22:16:18 +0800

    feat: add 141. Linked List Cycle

--- leetcode/solution.go
+++ leetcode/solution.go
@@ -187,6 +187,19 @@ func pathSum(root *datastructures.TreeNode, targetSum int) (ans [][]int) {
 	return
 }
 
+// 141. Linked List Cycle
+func hasCycle(head *datastructures.ListNode) bool {
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if slow == fast {
+			return true
+		}
+	}
+	return false
+}
+
 // 144.Binary Tree Preorder Traversal
 func preorderTraversal(root *datastructures.TreeNode) (res []int) {
 	var stack []*datastructures.TreeNode
