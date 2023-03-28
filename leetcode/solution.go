de0d45c819dd2bc6db5cee1b95393327c3af3b9a
trickyrat <wangjian0513@outlook.com>
2023-03-29 00:00:53 +0800

    feat: add 33. Search in Rotated Sorted Array

--- leetcode/solution.go
+++ leetcode/solution.go
@@ -182,6 +182,43 @@ func removeDuplicates(nums []int) int {
 	return slow
 }
 
+// 33. Search in Rotated Sorted Array
+func search(nums []int, target int) int {
+	n := len(nums)
+	if n == 0 {
+		return -1
+	}
+	if n == 1 {
+		if target == nums[0] {
+
+			return 0
+		} else {
+			return -1
+		}
+	}
+	left, right := 0, n-1
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] == target {
+			return mid
+		}
+		if nums[0] <= nums[mid] {
+			if nums[0] <= target && target < nums[mid] {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		} else {
+			if nums[mid] < target && target <= nums[n-1] {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		}
+	}
+	return -1
+}
+
 // 35. Search Insert Position
 func searchInsert(nums []int, target int) int {
 	n := len(nums)
